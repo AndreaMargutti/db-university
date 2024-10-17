975568603853d0d0cbc0fe3ebb348517bea00a3f
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-17 15:26:58 +0200

    third groupby

--- query-groupby-join.sql
+++ query-groupby-join.sql
@@ -1,3 +1,4 @@
+-- # GROUPBY
 -- Contare quanti iscritti ci sono stati ogni anno
 SELECT COUNT(*) AS 'iscritti'
 FROM `students` 
@@ -7,3 +8,10 @@ GROUP BY (YEAR(`enrolment_date`));
 SELECT COUNT(`id`) as 'Number', `office_address`
 FROM `teachers`
 GROUP BY (`office_address`);
+
+-- Calcolare la media dei voti di ogni appello d'esame
+SELECT AVG(`vote`) AS 'vote', `exam_id`
+FROM `exam_student`
+GROUP BY (`exam_id`);
+
+-- Contare quanti corsi di laurea ci sono per ogni dipartimento

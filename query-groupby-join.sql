29f26acfbf1aff82dc52b38a21b573fb1acd6bf3
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-17 15:03:32 +0200

    second groupby

--- query-groupby-join.sql
+++ query-groupby-join.sql
@@ -2,3 +2,8 @@
 SELECT COUNT(*) AS 'iscritti'
 FROM `students` 
 GROUP BY (YEAR(`enrolment_date`));
+
+-- Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(`id`) as 'Number', `office_address`
+FROM `teachers`
+GROUP BY (`office_address`);

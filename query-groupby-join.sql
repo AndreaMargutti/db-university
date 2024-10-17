d18b86dad63fcc528d1fc986b8c030dc52108cae
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-17 16:19:54 +0200

    Esercizio Finito (NO BONUS)

--- query-groupby-join.sql
+++ query-groupby-join.sql
@@ -54,3 +54,10 @@ JOIN `course_teacher` ON `course_teacher`.`course_id` = `courses`.`id`
 JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id`;
 
 -- Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT * 
+FROM `teachers`
+JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses` ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees` ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`id` = 5;
